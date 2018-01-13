ac114e88fa04ef34debe1589b74b6cc9be4eeaa0
Chia T <tham_oh@hotmail.com>
2018-01-13 00:02:08 -0600

    first commit

---
+++ 05-ColorPicker/server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def index1():
+    return render_template('index.html')
+
+@app.route('/',methods=['POST'])
+def index2():
+    print "*----------------"
+    print request.form['xred']
+    print request.form['xgreen']
+    print request.form['xblue']
+    return render_template('index.html',xred=request.form['xred'],xgreen=request.form['xgreen'],xblue=request.form['xblue'])
+
+app.run(debug=True)
